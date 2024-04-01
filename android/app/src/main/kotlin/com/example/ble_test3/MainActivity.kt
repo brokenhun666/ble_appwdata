092e6cfd6ac92d6f3ab54906f55608be8f26236a
Roland Pintér <MCJZT9@pte.hu>
2024-04-01 13:14:31 +0200

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/ble_test3/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.ble_test3
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,185 @@
+import 'dart:typed_data';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_reactive_ble/flutter_reactive_ble.dart';
+import 'package:permission_handler/permission_handler.dart';
+
+void main() {
+  return runApp(MaterialApp(home: BLEDevicesScreen()));
+}
+
+class BLEDevicesScreen extends StatefulWidget {
+  @override
+  _BLEDevicesScreenState createState() => _BLEDevicesScreenState();
+}
+
+class _BLEDevicesScreenState extends State<BLEDevicesScreen> {
+  final flutterReactiveBle = FlutterReactiveBle();
+  List<DiscoveredDevice> devices = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _requestPermission().then((_) {
+      flutterReactiveBle.scanForDevices(withServices: []).listen((device) {
+        setState(() {
+          if(!devices.contains(device)) {
+            devices.add(device);
+          }          
+        });
+      });
+    });
+  }
+
+  Future<void> _requestPermission() async {
+    if (await Permission.location.request().isGranted) {
+      // Either the permission was already granted before or the user just granted it.
+    } else {
+      showDialog(
+        context: context,
+        builder: (BuildContext context) => AlertDialog(
+          title: const Text('Location Permission'),
+          content: const Text('This app needs location permission to scan for BLE devices.'),
+          actions: <Widget>[
+            TextButton(
+              child: const Text('OK'),
+              onPressed: () => Navigator.of(context).pop(),
+            ),
+          ],
+        ),
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('BLE Devices'),
+      ),
+      body: ListView.builder(
+        itemCount: devices.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text(devices[index].name),
+            subtitle: Text(devices[index].id),
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => HomePage(deviceId: devices[index].id)),
+              );
+            },
+          );
+        },
+      ),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  final String deviceId;
+
+  const HomePage({Key? key, required this.deviceId}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  String xData = 'Loading...';
+  String yData = 'Loading...';
+  String zData = 'Loading...';
+
+  final flutterReactiveBle = FlutterReactiveBle();
+
+  final serviceUuid = Uuid.parse('082b91ae-e83c-11e8-9f32-f2801f1b9fd1');
+
+  final xCharacteristicUuid =
+      Uuid.parse('082b9438-e83c-11e8-9f32-f2801f1b9fd1');
+  final yCharacteristicUuid =
+      Uuid.parse('082b9622-e83c-11e8-9f32-f2801f1b9fd1');
+  final zCharacteristicUuid =
+      Uuid.parse('082b976c-e83c-11e8-9f32-f2801f1b9fd1');
+
+  void subscribeToCharacteristic() {
+    final xCharacteristic = QualifiedCharacteristic(
+        characteristicId: xCharacteristicUuid,
+        serviceId: serviceUuid,
+        deviceId: widget.deviceId);
+    final yCharacteristic = QualifiedCharacteristic(
+        characteristicId: yCharacteristicUuid,
+        serviceId: serviceUuid,
+        deviceId: widget.deviceId);
+    final zCharacteristic = QualifiedCharacteristic(
+        characteristicId: zCharacteristicUuid,
+        serviceId: serviceUuid,
+        deviceId: widget.deviceId);
+
+    flutterReactiveBle
+        .subscribeToCharacteristic(xCharacteristic)
+        .listen((data) {
+        setState(() {
+          xData = _bytesToFloat(data).toString();
+        });
+    });
+    flutterReactiveBle
+        .subscribeToCharacteristic(yCharacteristic)
+        .listen((data) {
+        setState(() {
+          yData = _bytesToFloat(data).toString();
+        });
+    });
+    flutterReactiveBle
+        .subscribeToCharacteristic(zCharacteristic)
+        .listen((data) {
+        setState(() {
+          zData = _bytesToFloat(data).toString();
+        });
+    });
+  }
+
+  double _bytesToFloat(List<int> data) {
+    final buffer = ByteData(4);
+    for (var i = 0; i < data.length; i++) {
+      buffer.setUint8(i, data[i]);
+    }
+    return buffer.getFloat32(0, Endian.little);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    subscribeToCharacteristic();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('BLE Characteristics display'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: <Widget>[
+            Card(
+              child: ListTile(
+                title: Text('X: $xData'),
+              ),
+            ),
+            Card(
+              child: ListTile(
+                title: Text('Y: $yData'),
+              ),
+            ),
+            Card(
+              child: ListTile(
+                title: Text('Z: $zData'),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
